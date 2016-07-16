af4b57ac1331248b13416e4602a4d88a2024a1f0
vishal <vishalsenthilkumaran05@gmail.com>
2016-07-17 00:03:15 +0530

    f

---
+++ vish.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class number
+{
+static Scanner in=new Scanner(System.in);
+static int num;
+public static void main(String[] args)
+{
+num=in.nextInt();
+ 
+    if ( num % 2 == 0 )
+        System.out.println("Entered number is even");
+     else
+        System.out.println("Entered number is odd");
+        }
+    }
